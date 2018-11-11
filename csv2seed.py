8481cf12543dd3d16f6ed6aebceadefd9df39caa
Erick Tucto <erick.tucto@outlook.com>
2018-11-11 15:03:20 -0500

    Add indented option

--- csv2seed.py
+++ csv2seed.py
@@ -10,17 +10,16 @@ def getFileName(file):
     return splitext(base)[0]
 
 
-def run(csv_file):
+def run(csv_file, indented=" " * 4, delimiter=";"):
     with open(csv_file, newline='') as csvfile:
-        spamreader = csv.DictReader(csvfile, delimiter=";")
+        spamreader = csv.DictReader(csvfile, delimiter=delimiter)
         seeder = ""
         for data in spamreader:
             attributes = list(data.keys())
-            intentation = " " * 4
             classModel = getFileName(csvfile.name).capitalize()
             content = f"{classModel}::create([\n"
             for attribute in attributes:
-                content += f'{intentation}"{attribute}" => "{data[attribute]}",\n'
+                content += f'{indented}"{attribute}" => "{data[attribute]}",\n'
             content = f"{content[:-2]}\n]);"
             seeder += f"{content}\n"
         seeder_file = f"{dirname(csvfile.name)}/{getFileName(csvfile.name)}.txt"
@@ -31,8 +30,10 @@ def run(csv_file):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('csvfile', help='Path to CSV file')
+    parser.add_argument('-i', '--indented', help='Indentation to spaces', type=int)
     args = parser.parse_args()
-    run(args.csvfile)
+    indented = args.indented if args.indented else " " * 4
+    run(args.csvfile, indented)
 
 if __name__ == '__main__':
     main()
