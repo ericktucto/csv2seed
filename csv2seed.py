1fe5c6fdec417a5e4895b15554c2c31948a0ca0c
Erick Tucto <erick.tucto@outlook.com>
2018-11-12 12:03:30 -0500

    Add model option

--- csv2seed.py
+++ csv2seed.py
@@ -10,14 +10,14 @@ def getFileName(file):
     return splitext(base)[0]
 
 
-def run(csv_file, indented=" " * 4, delimiter=";"):
+def run(csv_file, indented=" " * 4, delimiter=";", model=None):
     with open(csv_file, newline='') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=delimiter)
         seeder = ""
         for data in spamreader:
             attributes = list(data.keys())
-            classModel = getFileName(csvfile.name).capitalize()
-            content = f"{classModel}::create([\n"
+            model = getFileName(csvfile.name).capitalize() if not model else model
+            content = f"{model}::create([\n"
             for attribute in attributes:
                 content += f'{indented}"{attribute}" => "{data[attribute]}",\n'
             content = f"{content[:-2]}\n]);"
@@ -25,6 +25,7 @@ def run(csv_file, indented=" " * 4, delimiter=";"):
         seeder_file = f"{dirname(csvfile.name)}/{getFileName(csvfile.name)}.txt"
         f  = open(seeder_file, "w")
         f.write(seeder)
+        f.close()
 
 
 def main():
@@ -33,13 +34,15 @@ def main():
     parser.add_argument('-s', '--spaces', help='Indentation to spaces', type=int)
     parser.add_argument('-t', '--tabulation', help='Indentation to tabulation', action='store_true')
     parser.add_argument('-d', '--delimiter', help='Delimiter to columns, default = ;')
+    parser.add_argument('-m', '--model', help='Set name model')
     args = parser.parse_args()
 
     indented = " " * args.spaces if args.spaces else " " * 4
     indented = "\t" if args.tabulation else indented
-    delimiter = args.delimiter if args.delimiter else ";"
+    delimiter = args.delimiter or ";"
+    model = args.model
 
-    run(args.csvfile, indented, delimiter)
+    run(args.csvfile, indented, delimiter, model)
 
 if __name__ == '__main__':
     main()
