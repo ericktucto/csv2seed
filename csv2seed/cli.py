1d90c46778bd7da2da2dd72ccb825120aba6829c
Erick Tucto <erick.tucto@outlook.com>
2018-11-12 19:12:14 -0500

    Update cli.py - use click package

--- csv2seed/cli.py
+++ csv2seed/cli.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse
+import click
 from csv2seed.converter import run
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('csvfile', help='Path to CSV file')
-    parser.add_argument('-s', '--spaces', help='Indentation to spaces', type=int)
-    parser.add_argument('-t', '--tabulation', help='Indentation to tabulation', action='store_true')
-    parser.add_argument('-d', '--delimiter', help='Delimiter to columns, default = ;')
-    parser.add_argument('-m', '--model', help='Set name model')
-    args = parser.parse_args()
+@click.command()
+@click.argument('csvfile')
+@click.option('-s', '--spaces', help='Indentation to spaces', type=int,
+              default=4)
+@click.option('-t', '--tabulation', help='Indentation to tabulation',
+              is_flag=True)
+@click.option('-d', '--delimiter', help='Delimiter to columns, default = ;',
+              default=";")
+@click.option('-m', '--model', help='Set name model', default=None)
+def main(csvfile, spaces, tabulation, delimiter, model):
+    indented = "\t" if tabulation else " " * spaces
 
-    indented = " " * args.spaces if args.spaces else " " * 4
-    indented = "\t" if args.tabulation else indented
-    delimiter = args.delimiter or ";"
-    model = args.model
-
-    run(args.csvfile, indented, delimiter, model)
+    run(csvfile, indented, delimiter, model)
 
 
 if __name__ == '__main__':
