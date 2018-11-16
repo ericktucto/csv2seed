bc22d4000571908989e6069caae5aad553d7adfb
Erick Tucto <erick.tucto@outlook.com>
2018-11-15 19:05:47 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
+from os.path import dirname, abspath, join
+
+_here = abspath(dirname(__file__))
+with open(join(_here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='csv2seed',
-    version="0.1.0",
+    version='0.1.0',
+
+    url='https://github.com/ErickTucto/csv2seed',
     description='CSV file converter to Laravel Seeders',
+    long_description=long_description,
+    keywords=['csv', 'laravel', 'seeders', 'seed', 'converter', 'generator'],
+
+    license='MIT',
+
     author='Erick Tucto',
     author_email='erick.tucto@outlook.com',
-    url='https://ericktucto.com',
-    keywords=['csv', 'laravel', 'seeders', 'seed', 'converter', 'generator'],
+
     packages=['csv2seed'],
     entry_points={
-        'console_scripts': ['csv2seed = csv2seed.cli:main']
-    },
-    licence='MIT'
+        'console_scripts': ['csv2seed=csv2seed.cli:main']
+    }
 )
