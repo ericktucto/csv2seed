b75cae071d5aeaf9a15dd8d526657f9fba12c9a7
Erick Tucto <erick.tucto@outlook.com>
2018-12-04 19:28:28 -0500

    Simple run

--- csv2seed/converter.py
+++ csv2seed/converter.py
@@ -7,41 +7,41 @@ template = """{model}::create([
 """
 
 
-def getFileName(file):
-    base = basename(file)
-    return splitext(base)[0]
-
-
-def getAbsolutePath(file):
-    return dirname(abspath(file))
-
-
-def generateContent(indented, attributes, data):
-    return "\n".join([
-        '%s"%s" => "%s",' %
-        (indented, attribute, tuple(data)[index])
-        for index, attribute in enumerate(attributes)
-    ])
-
-
 def run(csv_file, indented=" " * 4, delimiter=";", model=None,
         attributes=None, has_header=False):
+
+    def generateContent(indented, attributes, data):
+        content = "\n".join([
+            '%s"%s" => "%s",' %
+            (indented, attribute, tuple(data)[index])
+            for index, attribute in enumerate(attributes)
+        ])
+        return content[:-1]  # Delete , end
+
+    def getFileName(file):
+        base = basename(file)
+        return splitext(base)[0]
+
+    def getAbsolutePath(file):
+        return dirname(abspath(file))
+
     with open(csv_file, newline='') as csvfile:
         seeder = ""
         file_name = getFileName(csvfile.name)
         model = model or file_name.capitalize()
         include_header = attributes and not has_header
-        reader = csv.DictReader(csvfile, delimiter=delimiter)
-        for data in reader:
+        for data in csv.DictReader(csvfile, delimiter=delimiter):
             content = ""
             attributes = attributes or tuple(data)
             if include_header:
-                content = generateContent(indented, attributes, data.keys())
-                seeder += template.format(model=model, attributes=content[:-1])
+                values = data.keys()
+                content = generateContent(indented, attributes, values)
+                seeder += template.format(model=model, attributes=content)
                 include_header = False
                 content = ""
-            content = generateContent(indented, attributes, data.values())
-            seeder += template.format(model=model, attributes=content[:-1])
+            values = data.values()
+            content = generateContent(indented, attributes, values)
+            seeder += template.format(model=model, attributes=content)
         seeder_file = "%s/%s.txt" % (getAbsolutePath(csv_file), file_name)
         f = open(seeder_file, "w")
         f.write(seeder)
